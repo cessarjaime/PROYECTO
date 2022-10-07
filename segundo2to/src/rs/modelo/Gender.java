5cb07a76172e99b9b6813ff225ba85e6dd4522f6
cessarjaime <115181645+cessarjaime@users.noreply.github.com>
2022-10-07 17:35:59 -0300

    Create Gender.java
    Clase enum Gender. para que las variables de genero sean solo estas 3. H: hombre, M: mujer, NB: no binario

---
+++ segundo2to/src/rs/modelo/Gender.java
@@ -0,0 +1,16 @@
+package modelo;
+
+public enum Gender {
+	M("mujer"), H("hombre"), NB ("no binario");
+
+	private String genero;
+
+	Gender(String genero) {
+		this.genero = genero;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+	
+}
