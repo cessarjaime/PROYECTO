a94b00140e138395a6e775318cf23297d9c37900
cessarjaime <115181645+cessarjaime@users.noreply.github.com>
2022-11-29 20:12:32 -0300

    Update Calendario.java

--- segundo2to/src/rs/util/Calendario.java
+++ segundo2to/src/rs/util/Calendario.java
@@ -1,12 +1,22 @@
-package rs.util;
 
 import java.time.LocalDate;
 
+/**
+ * Clase accesoria calendario
+ * @author Jaime, Cesar; Camacho, Cristian
+ *
+ */
 public class Calendario {
 
+	
 	public Calendario() {
 	}
 
+	/**
+	 * obtiene edad en anios 
+	 * @param fechaNacimiento
+	 * @return anios
+	 */
 	public static int getTiempo(LocalDate fechaNacimiento) {
 		int anios = LocalDate.now().getYear() - fechaNacimiento.getYear();
 		int meses = LocalDate.now().getMonthValue() - fechaNacimiento.getMonthValue();
