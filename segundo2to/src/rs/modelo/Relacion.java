daafb05463d7330f53e36c57ad86ae781dd09d9b
cessarjaime <115181645+cessarjaime@users.noreply.github.com>
2022-11-29 19:07:09 -0300

    Update Relacion.java

--- segundo2to/src/rs/modelo/Relacion.java
+++ segundo2to/src/rs/modelo/Relacion.java
@@ -4,7 +4,11 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 import rs.util.Calendario;
-
+/**
+ * Relacion entre usuarios
+ * @author Camacho, Cristian; Jaime,Cesar
+ *
+ */
 public class Relacion {
 	private Usuario usuario1;
 	private Usuario usuario2;
@@ -12,6 +16,14 @@ public class Relacion {
 	private int likes;
 	private LocalDate fechaAmistad;
 	
+	/**
+	 * constructor de relacion
+	 * @param usuario1
+	 * @param usuario2
+	 * @param interaccion entero que cuantifica la interaccion entre usuarios
+	 * @param likes cantidad de likes en la relacion
+	 * @param fechaAmistad fecha en que su relación fue creada
+	 */
 	public Relacion(Usuario usuario1, Usuario usuario2, int interaccion, int likes,LocalDate fechaAmistad) {
 		super();
 		this.usuario1 = usuario1;
@@ -20,55 +32,107 @@ public class Relacion {
 		this.likes = likes;
 		this.fechaAmistad = fechaAmistad;
 	}
+
+	/**
+	 * constructor relacion sin parametros
+	 */
 	public Relacion() {
 		
 	}
 
+	/**
+	 * obtiene usuario1
+	 * @return usuario1
+	 */
 	public Usuario getUsuario1() {
 		return usuario1;
 	}
 
+	/**
+	 * establece usuario1
+	 * @param usuario1
+	 */
 	public void setUsuario1(Usuario usuario1) {
 		this.usuario1 = usuario1;
 	}
 
+	/**
+	 * obtiene usuario2
+	 * @return usuario2
+	 */
 	public Usuario getUsuario2() {
 		return usuario2;
 	}
 
+	/**
+	 * establece usuario2
+	 * @param usuario2
+	 */
 	public void setUsuario2(Usuario usuario2) {
 		this.usuario2 = usuario2;
 	}
 
+	/**
+	 * obtiene interaccion de los usuarios
+	 * @return entero que cuantifica interaccion
+	 */
 	public int getInteraccion() {
 		return interaccion;
 	}
 
+	/**
+	 * establece interaccion
+	 * @param interaccion
+	 */
 	public void setInteraccion(int interaccion) {
 		this.interaccion = interaccion;
 	}
 
+	/**
+	 * obtiene cantidad de likes
+	 * @return cantidad de likes
+	 */
 	public int getLikes() {
 		return likes;
 	}
 
+	/**
+	 * establece cantidad de likes
+	 * @param likes
+	 */
 	public void setLikes(int likes) {
 		this.likes = likes;
 	}
 	
+	/**
+	 * obtiene fecha que comenzó la relacion
+	 * @return fecha
+	 */
 	public LocalDate getFechaAmistad() {
 	
 		return fechaAmistad;
 	}
 
 
+	/**
+	 * establece fecha que comenzó la relación
+	 * @param fechaAmistad
+	 */
 	public void setFechaAmistad(LocalDate fechaAmistad) {
 		this.fechaAmistad = fechaAmistad;
 	}
+	
+	/**
+	 * obtiene el tiempo desde que comenzó la relacion
+	 * @return
+	 */
 	public int getTiempoAmistad() {
         return Calendario.getTiempo(fechaAmistad);
 	}
 
+	/**
+	 * toString relacion
+	 */
 	@Override
 	public String toString() {
 		return "Relacion ["+ usuario1 + " " + usuario2 + " " + interaccion + " "
@@ -94,11 +158,4 @@ public class Relacion {
 		return Objects.equals(usuario1, other.usuario2) && Objects.equals(usuario2, other.usuario1);
 	}
 
-
-	
-	
-	
-	
-		
-	
 }
