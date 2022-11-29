c395da810753054965298d254980022a36bafa05
cessarjaime <115181645+cessarjaime@users.noreply.github.com>
2022-11-29 20:08:56 -0300

    Update Subject.java

--- segundo2to/src/rs/negocio/Subject.java
+++ segundo2to/src/rs/negocio/Subject.java
@@ -3,18 +3,33 @@ package rs.negocio;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * sujeto observador de cambio de estados
+ * @author usuario
+ *
+ */
 public class Subject {
 
 	private List<Observer> observers = new ArrayList<Observer>();
 	
+	/**
+	 * notifica los cambios de estado
+	 */
 	public void refresh() {				
 		notifyAllObservers();
 	}
 	
+	/**
+	 * adjunta observador
+	 * @param observer
+	 */
 	public void attach(Observer observer) {
 		observers.add(observer);
 	}
 
+	/**
+	 * actualiza los cambios de estado
+	 */
 	public void notifyAllObservers() {
 		for (Observer observer : observers) {
 			observer.update();
