856e114d06805c58ba29480febb1606c6509cd28
cessarjaime <115181645+cessarjaime@users.noreply.github.com>
2022-11-29 20:05:43 -0300

    Update Usuario.java

--- segundo2to/src/rs/modelo/Usuario.java
+++ segundo2to/src/rs/modelo/Usuario.java
@@ -5,6 +5,11 @@ import java.util.Objects;
 
 import rs.util.Calendario;
 
+/**
+ * Clase usuario
+ * @author Jaime, Cesar; Camacho, Cristian
+ *
+ */
 public class Usuario {
 	private String id;
 	private String nombre;
@@ -14,8 +19,16 @@ public class Usuario {
 	private String estadoCivil;
 	private String nivelAcademico;
 
-
-
+	/**
+	 * constructor de clase usuario
+	 * @param id identificador unico de usuario
+	 * @param nombre 
+	 * @param genero
+	 * @param ciudad
+	 * @param fechaNacimiento
+	 * @param estadoCivil
+	 * @param nivelAcademico
+	 */
 	public Usuario(String id, String nombre, Gender genero, String ciudad,LocalDate fechaNacimiento,
 			String estadoCivil, String nivelAcademico) {
 		super();
@@ -27,66 +40,122 @@ public class Usuario {
 		this.estadoCivil = estadoCivil;
 		this.nivelAcademico = nivelAcademico;
 	}
+	
+	/**
+	 * constructor usuario sin parametros
+	 */
 	public Usuario() {
 		
 	}
 
+	/**
+	 * obtiene el nombre
+	 * @return nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * establece el nombre
+	 * @param nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * obtiene el id
+	 * @return id
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * establece el id
+	 * @param id
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 	
-	public enum genero {
-		hombre, mujer;
-	}
-
+	/**
+	 * obtiene el genero
+	 * @return genero
+	 */
 	public Gender getGenero() {
 		return genero;
 	}
 
+	/**
+	 * establece el genero
+	 * @param genero
+	 */
 	public void setGenero(Gender genero) {
 		this.genero = genero;
 	}
 
+	/**
+	 * obtiene ciudad
+	 * @return ciudad
+	 */
 	public String getCiudad() {
 		return ciudad;
 	}
 
+	/**
+	 * establece ciudad
+	 * @param ciudad
+	 */
 	public void setCiudad(String ciudad) {
 		this.ciudad = ciudad;
 	}
     
+	/**obtiene fecha de nacimiento
+	 * 
+	 * @return fecha de nacimiento
+	 */
 	public LocalDate getFechaNacimiento() {
 		return fechaNacimiento;
 	}
 
+	/**
+	 * establece fecha de nacimiento
+	 * @param fechaNacimiento
+	 */
 	public void setFechaNacimiento(LocalDate fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
 	}
 
+	/**
+	 * obtiene estado civil
+	 * @return estado civil
+	 */
 	public String getEstadoCivil() {
 		return estadoCivil;
 	}
 
+	/**
+	 * establece estado civil
+	 * @param estadoCivil
+	 */
 	public void setEstadoCivil(String estadoCivil) {
 		this.estadoCivil = estadoCivil;
 	}
 
+	/**
+	 * obtiene nivel academico
+	 * @return nivel academico
+	 */
 	public String getNivelAcademico() {
 		return nivelAcademico;
 	}
 
+	/**
+	 * establece nivel academico
+	 * @param nivelAcademico
+	 */
 	public void setNivelAcademico(String nivelAcademico) {
 		this.nivelAcademico = nivelAcademico;
 	}
