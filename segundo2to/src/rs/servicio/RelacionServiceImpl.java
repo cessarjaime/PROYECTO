a8df126b5d00ca7022720f0f3dbc7771de637c17
cessarjaime <115181645+cessarjaime@users.noreply.github.com>
2022-11-29 20:10:19 -0300

    Update RelacionServiceImpl.java

--- segundo2to/src/rs/servicio/RelacionServiceImpl.java
+++ segundo2to/src/rs/servicio/RelacionServiceImpl.java
@@ -5,30 +5,54 @@ import rs.conexion.Factory;
 import rs.dao.RelacionDAO;
 import rs.modelo.Relacion;
 
+/**
+ * implementacion del servicio de relaciones
+ * @author Jaime, Cesar; Camacho, Cristian
+ *
+ */
 public class RelacionServiceImpl implements RelacionService {
 
 	private RelacionDAO relacionDAO; 
-		
+	
+	/**
+	 * obtiene la interfaz RelacionDAO de factory
+	 */
 	public RelacionServiceImpl(){
 		relacionDAO = (RelacionDAO) Factory.getInstancia("RELACIONES");
 	}
 	
+	/**
+	 * inserta relacion 
+	 * @param relacion
+	 */
 	@Override
 	public void insertar(Relacion relacion) {
 		relacionDAO.insertar(relacion);				
 	}
 
+	/**
+	 * actualiza relacion
+	 * @param relacion
+	 */
 	@Override
 	public void actualizar(Relacion relacion) {
 		relacionDAO.actualizar(relacion);						
 	}
 
+	/**
+	 * borra relacion
+	 * @param relacion
+	 */
 	@Override
 	public void borrar(Relacion relacion) {
 		relacionDAO.borrar(relacion);
 		
 	}
 
+	/**
+	 * obtiene lista de relaciones
+	 * @return lista relaciones
+	 */
 	@Override
 	public List<Relacion> buscarTodos() {
 		return relacionDAO.buscarTodos();
