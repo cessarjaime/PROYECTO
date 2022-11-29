9511190ddad3e990eed030d9ef6bc10ddd6b2f8d
cessarjaime <115181645+cessarjaime@users.noreply.github.com>
2022-11-29 18:54:35 -0300

    Update RelacionDAO.java

--- segundo2to/src/rs/dao/RelacionDAO.java
+++ segundo2to/src/rs/dao/RelacionDAO.java
@@ -3,6 +3,11 @@ package rs.dao;
 import java.util.List;
 import rs.modelo.Relacion;
 
+/**
+ * Interfaz de objeto de acceso a datos de relaciones
+ * @author Camacho, Cristian; Jaime, Cesar
+ *
+ */
 public interface RelacionDAO {
 	
 	void insertar(Relacion relacion);
