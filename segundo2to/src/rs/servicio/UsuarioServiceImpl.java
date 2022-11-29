d171bf3dffb03c4912635540655b5c067c9d920c
cessarjaime <115181645+cessarjaime@users.noreply.github.com>
2022-11-29 20:11:31 -0300

    Update UsuarioServiceImpl.java

--- segundo2to/src/rs/servicio/UsuarioServiceImpl.java
+++ segundo2to/src/rs/servicio/UsuarioServiceImpl.java
@@ -5,30 +5,54 @@ import rs.conexion.Factory;
 import rs.dao.UsuarioDAO;
 import rs.modelo.Usuario;
 
+/**
+ * implementacion del servicio de usuarios
+ * @author Jaime, Cesar; Camacho, Cristian
+ *
+ */
 public class UsuarioServiceImpl implements UsuarioService {
 
 	private UsuarioDAO usuarioDAO; 
 		
+	/**
+	 * obtiene la interfaz UsuarioDAO de factory
+	 */
 	public UsuarioServiceImpl(){
 		usuarioDAO = (UsuarioDAO) Factory.getInstancia("USUARIOS");
 	}
 	
+	/**
+	 * inserta usuario 
+	 * @param usuario
+	 */
 	@Override
 	public void insertar(Usuario usuario) {
 		usuarioDAO.insertar(usuario);				
 	}
 
+	/**
+	 * actualiza usuario
+	 * @param usuario
+	 */
 	@Override
 	public void actualizar(Usuario usuario) {
 		usuarioDAO.actualizar(usuario);						
 	}
 
+	/**
+	 * borra usuario
+	 * @param usuario
+	 */
 	@Override
 	public void borrar(Usuario usuario) {
 		usuarioDAO.borrar(usuario);
 		
 	}
 
+	/**
+	 * obtiene lista de usuario
+	 * @return lista usuarios
+	 */
 	@Override
 	public List<Usuario> buscarTodos() {
 		return usuarioDAO.buscarTodos();
